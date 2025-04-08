0ebc19635bfcd03f611f5007f4fd376a40e2a2cf
romanvillagran <romanvillagran386@gmail.com>
2025-04-07 22:42:31 -0700

    Add project files.

---
+++ COMP003B.LectureActivity4/Models/Article.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class Article
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Title { get; set; }
+
+    [Required]
+    [StringLength(300)]
+    public string Summary { get; set; }
+}
